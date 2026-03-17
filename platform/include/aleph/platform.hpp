593ddbe3481e75df0161ff6dea6af3fc0c6a4838
Khrysys <goalieziper74@gmail.com>
2026-03-17 17:27:46 -0400

    Remove ugly macros from logging, revert to STL variants, use spdlog instead

--- platform/include/aleph/platform.hpp
+++ platform/include/aleph/platform.hpp
@@ -8,6 +8,5 @@
 #include "platform/allocation.hpp"
 #include "platform/compiler.hpp"
 #include "platform/intrinsics.hpp"
-#include "platform/logging.hpp"
-#include "platform/print_info.hpp"
+#include "platform/log_info.hpp"
 // NOLINTEND
