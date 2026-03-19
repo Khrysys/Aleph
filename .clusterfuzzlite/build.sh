bad1332adf85ae510bd071a5b0f9bb06e22588d9
Khrysys <goalieziper74@gmail.com>
2026-03-18 23:32:33 -0400

    Trying a different setup for it

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,9 +1,21 @@
 #!/bin/bash -eu
 
+# build project
+# e.g.
+# ./autogen.sh
+# ./configure
+# make -j$(nproc) all
+
 cd $SRC/aleph
 
 conan build . \
     --build=missing \
-    --profile=.github/profiles/fuzzing-host
+    -c tools.build:cxxflags=$CXXFLAGS
+    -c tools.build:compiler_executables=$CXX
+    -c tools.cmake.cmaketoolchain:extra_variables={'Aleph_BUILD_FUZZING', 'ON'}
 
-find build -name "fuzz_*" -type f -executable | xargs -I{} cp {} $OUT/
+# build fuzzers
+# e.g.
+# $CXX $CXXFLAGS -std=c++11 -Iinclude \
+#     /path/to/name_of_fuzzer.cc -o $OUT/name_of_fuzzer \
+#     $LIB_FUZZING_ENGINE /path/to/library.a
